59eb6ccfe0e67743c59894ab5f6b7e449fcd3d59
Kihoru <thomas.graph.rigal@gmail.com>
2017-08-29 12:53:35 +0200

    fix offset 0 on index

--- resources/views/school/parts/index.blade.php
+++ resources/views/school/parts/index.blade.php
@@ -5,6 +5,7 @@
 <main class="main-content-accueil">
     <div class="colonne-centrale">
         <div class="actualite">
+            @if(isset($lastPost[0]) && !empty($lastPost[0]))
             <h2 class="titre">{{$lastPost[0]->title}}</h2>
             <img class="image" src="{{url('/upload/posts/'.$lastPost[0]->url_thumbnail)}}">
             <p class="texte">{{$lastPost[0]->abstract}}</p>
@@ -14,6 +15,9 @@
             <h3 class="auteur">Auteur : <span>{{$lastPost[0]->user->username}}</span></h3>
             <p class="date">Publié le : {{$lastPost[0]->created_at}}</p>
             <p class="commentaire"><i class="fa fa-comments" aria-hidden="true"></i> {{count($lastPost[0]->comments)}} commentaire(s) réagissez !</p>
+            @else
+            <h2 class="titre">Aucuns articles n'a été écris.</h2>
+            @endif
         </div>
         <div class="fil-actu">
             @foreach($latestPosts as $post)
